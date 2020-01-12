f2ce33fbfe7f34977c638e8ebc8b8167140e5cd0
mztikk <nevigskcuforez@gmail.com>
2020-01-12 15:59:24 +0100

    implemented copyable in SigmakerOptions

--- AoBSigmaker/Options/SigmakerOptions.cs
+++ AoBSigmaker/Options/SigmakerOptions.cs
@@ -3,14 +3,31 @@ using System.Threading.Tasks;
 
 namespace AoBSigmaker.Options
 {
-    public class SigmakerOptions
+    public class SigmakerOptions : Copyable<SigmakerOptions>
     {
-        public bool TrustValidity { get; set; }
+        public const string OptionsFile = "aobsigmaker.cfg";
+
+        private bool _trustValidity;
+
+        public bool TrustValidity
+        {
+            get => _trustValidity;
+            set
+            {
+                if (value != _trustValidity)
+                {
+                    _trustValidity = value;
+                    NotifOfPropertyChange();
+                }
+            }
+        }
 
         public async Task WriteToFile(FileInfo file) => await JsonHelper.WriteToFile(file, this).ConfigureAwait(false);
         public async Task WriteToFile(string filepath) => await JsonHelper.WriteToFile(filepath, this).ConfigureAwait(false);
+        public async Task WriteToFile() => await JsonHelper.WriteToFile(OptionsFile, this).ConfigureAwait(false);
 
         public static async Task<SigmakerOptions> LoadFromFile(FileInfo file) => await JsonHelper.LoadFromFile<SigmakerOptions>(file).ConfigureAwait(false);
         public static async Task<SigmakerOptions> LoadFromFile(string filepath) => await JsonHelper.LoadFromFile<SigmakerOptions>(filepath).ConfigureAwait(false);
+        public static async Task<SigmakerOptions> LoadFromFile() => await JsonHelper.LoadFromFile<SigmakerOptions>(OptionsFile).ConfigureAwait(false);
     }
 }
