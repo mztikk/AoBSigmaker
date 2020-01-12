70282e489f250d0c788b3cd09eb546dd9912d0a7
mztikk <nevigskcuforez@gmail.com>
2020-01-12 15:59:52 +0100

    added options to optionsview and viewmodel

--- AoBSigmaker/ViewModels/OptionsViewModel.cs
+++ AoBSigmaker/ViewModels/OptionsViewModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using AoBSigmaker.Options;
+using Stylet;
 
 namespace AoBSigmaker.ViewModels
 {
-    public class OptionsViewModel
+    public class OptionsViewModel : Screen
     {
+        private readonly SigmakerOptions _options;
+
+        public OptionsViewModel(SigmakerOptions options)
+        {
+            _options = options;
+            Options = new SigmakerOptions();
+            _options.CopyTo(Options);
+        }
+
+        public SigmakerOptions Options { get; set; }
+
+        public async Task Save()
+        {
+            Options.CopyTo(_options);
+            await _options.WriteToFile().ConfigureAwait(false);
+            Execute.OnUIThread(() => RequestClose(true));
+        }
+
+        public void Cancel() => RequestClose(false);
     }
 }
