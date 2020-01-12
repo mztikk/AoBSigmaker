8e15dcd35356b11a690f733bda27ded4db1142f7
mztikk <nevigskcuforez@gmail.com>
2020-01-12 13:54:12 +0100

    added SigmakerOptions

---
+++ AoBSigmaker/Options/SigmakerOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AoBSigmaker.Options
+{
+    public class SigmakerOptions
+    {
+        public bool TrustValidity { get; set; }
+
+        public async Task WriteToFile(FileInfo file) => await JsonHelper.WriteToFile(file, this).ConfigureAwait(false);
+        public async Task WriteToFile(string filepath) => await JsonHelper.WriteToFile(filepath, this).ConfigureAwait(false);
+
+        public static async Task<SigmakerOptions> LoadFromFile(FileInfo file) => await JsonHelper.LoadFromFile<SigmakerOptions>(file).ConfigureAwait(false);
+        public static async Task<SigmakerOptions> LoadFromFile(string filepath) => await JsonHelper.LoadFromFile<SigmakerOptions>(filepath).ConfigureAwait(false);
+    }
+}
