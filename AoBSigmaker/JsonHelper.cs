b555f8b104989714bd55ff025e5cc00070e74821
mztikk <nevigskcuforez@gmail.com>
2020-01-12 13:51:19 +0100

    added JsonHelper

---
+++ AoBSigmaker/JsonHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AoBSigmaker
+{
+    internal static class JsonHelper
+    {
+        public static async Task<T> LoadFromFile<T>(FileInfo file)
+        {
+            using (FileStream stream = file.OpenRead())
+            {
+                return await JsonSerializer.DeserializeAsync<T>(stream).ConfigureAwait(false);
+            }
+        }
+
+        public static async Task<T> LoadFromFile<T>(string filepath) => await LoadFromFile<T>(new FileInfo(filepath)).ConfigureAwait(false);
+
+        public static async Task WriteToFile<T>(FileInfo file, T value)
+        {
+            using (FileStream stream = file.Open(FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+            {
+                await JsonSerializer.SerializeAsync(stream, value).ConfigureAwait(false);
+            }
+        }
+
+        public static async Task WriteToFile<T>(string filepath, T value) => await WriteToFile(new FileInfo(filepath), value).ConfigureAwait(false);
+    }
+}
